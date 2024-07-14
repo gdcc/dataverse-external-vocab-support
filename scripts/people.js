2fd7fd0fd0859be75fe0defe5762de2c99c911f2
Jim Myers <qqmyers@hotmail.com>
2024-07-14 13:43:39 -0400

    Multiple changes to support parent/managed fields, fixes/improvements
    support managed fields
    use util methods for storing local data
    fix error/plain text handling
    fix behavior with 0 lookup results
    fix prioritization of locally stored vals
    change display to match family, given in author field
    change separator to ;

--- scripts/people.js
+++ scripts/people.js
@@ -1,5 +1,6 @@
 var personSelector = "span[data-cvoc-protocol='orcid']";
 var personInputSelector = "input[data-cvoc-protocol='orcid']";
+var orcidPrefix="orcid:";
 
 $(document).ready(function() {
     expandPeople();
@@ -29,8 +30,14 @@ function expandPeople() {
                 success: function(person, status) {
                     //If found, construct the HTML for display
                     var name = person.name['family-name'].value + ", " + person.name['given-names'].value;
-                    var html = "<a href='https://orcid.org/" + id + "' target='_blank' rel='noopener' >" + name + "</a>";
-                    personElement.innerHTML = html;
+                    var html = "<a href='https://orcid.org/" + id + "' target='_blank' rel='noopener' >" + name + "</a> ";
+                    $(personElement).hide();
+                    let sibs = $(personElement).siblings("[data-cvoc-index='" + $(personElement).attr('data-cvoc-index') + "']");
+                    if(sibs.length==0) {
+                        $(personElement).parent().prepend(html);
+                    } else {
+                        sibs.eq(0).before(html);
+                    }
                     //If email is public, show it using the jquery popover functionality
                     if (person.emails.email.length > 0) {
                         $(personElement).popover({
@@ -45,14 +52,17 @@ function expandPeople() {
                             $(this).popover('hide');
                         };
                     }
-                    //Store the most recent 100 ORCIDs - could cache results, but currently using this just to prioritized recently used ORCIDs in search results
-                    if (localStorage.length > 100) {
-                        localStorage.removeItem(localStorage.key(0));
-                    }
-                    localStorage.setItem(id, name);
+                    //Store the most recent ORCIDs - could cache results, but currently using this just to prioritized recently used ORCIDs in search results
+                    storeValue(orcidPrefix, id, name);
                 },
-                failure: function(jqXHR, textStatus, errorThrown) {
-                    //Generic logging - don't need to do anything if 404 (leave display as is)
+                error: function(jqXHR, textStatus, errorThrown) {
+                    $(personElement).show();
+                    let index = $(personElement).attr('data-cvoc-index');
+                    if(index !== undefined) {
+                        $(personElement).siblings().show();
+                        $(personElement).siblings("[data-cvoc-index='" + index + "']").removeClass('hidden');
+                    }
+                    //Generic logging if not 404
                     if (jqXHR.status != 404) {
                         console.error("The following error occurred: " + textStatus, errorThrown);
                     }
@@ -63,22 +73,40 @@ function expandPeople() {
 }
 
 function updatePeopleInputs() {
-    //For each input element within personInputSelector elements 
+    console.log("Local people.js");
+    //For each input element within personInputSelector elements
     $(personInputSelector).each(function() {
         var personInput = this;
         if (!personInput.hasAttribute('data-person')) {
             //Random identifier
             let num = Math.floor(Math.random() * 100000000000);
+            $(personInput).attr('data-person', num);
 
-            //Hide the actual input and give it a data-person number so we can find it
-            $(personInput).hide();
+            let parentField = $(personInput).attr('data-cvoc-parent');
+            var parent = $(personInput).closest("[data-cvoc-parentfield='" + parentField + "']");
+
+            let hasParentField = $("[data-cvoc-parentfield='" + parentField + "']").length > 0;
+            let managedFields = {};
+            if(hasParentField) {
+                managedFields = JSON.parse($(personInput).attr('data-cvoc-managedfields'));
+                if(Object.keys(managedFields).length > 0) {
+                    //Hide managed fields
+                    $(parent).find("input[data-cvoc-managed-field='" + managedFields.personName + "']").hide();
+                    $(parent).find("[data-cvoc-managed-field='" + managedFields.idType + "']").parent().hide();
+                }
+                //Hide the actual input and give it a data-person number so we can find it
+                $(personInput).parent().hide();
+            } else {
+                $(personInput).hide();
+            }
+
             $(personInput).attr('data-person', num);
             //Todo: if not displayed, wait until it is to then create the select 2 with a non-zero width
 
             //Add a select2 element to allow search and provide a list of choices
             var selectId = "personAddSelect_" + num;
-            $(personInput).after(
-                '<select id=' + selectId + ' class="form-control add-resource select2" tabindex="-1" aria-hidden="true">');
+            $(personInput).parent().parent().children('div').eq(0).append(
+              '<select id=' + selectId + ' class="form-control add-resource select2" tabindex="-1" aria-hidden="true">');
             $("#" + selectId).select2({
                 theme: "classic",
                 tags: $(personInput).attr('data-cvoc-allowfreetext'),
@@ -138,17 +166,22 @@ function updatePeopleInputs() {
                         'Accept': 'application/json'
                     },
                     processResults: function(data, page) {
+                        let newItems = data['expanded-result'];
+                        if(newItems==null) {
+                          console.log("Returning empty array");
+                          return { results: [] };
+                        }
                         return {
                             results: data['expanded-result']
                                 //Sort to bring recently used ORCIDS to the top of the list
-                                .sort((a, b) => (localStorage.getItem(b['orcid-id'])) ? 1 : 0)
+                                .sort((a, b) => Number(getValue(orcidPrefix, b['orcid-id']).name != null) - Number(getValue(orcidPrefix, a['orcid-id']).name != null))
                                 .map(
                                     function(x) {
                                         return {
-                                            text: x['given-names'] + " " + x['family-names'] +
-                                                ", " +
+                                            text: x['family-names'] + ", " + x['given-names'] +
+                                                "; " +
                                                 x['orcid-id'] +
-                                                ((x.email.length > 0) ? ", " + x.email[0] : ""),
+                                                ((x.email.length > 0) ? "; " + x.email[0] : ""),
                                             id: x['orcid-id'],
                                             //Since clicking in the selection re-opens the choice list, one has to use a right click/open in new tab/window to view the ORCID page
                                             //Using title to provide that hint as a popup
@@ -171,23 +204,31 @@ function updatePeopleInputs() {
                         'Accept': 'application/json'
                     },
                     success: function(person, status) {
-                        var name = person.name['given-names'].value + " " + person.name['family-name'].value;
-                        var text = name + ", " + id;
+                        var name = person.name['family-name'].value + ", " +  person.name['given-names'].value;
+                        var text = name + "; " + id;
                         if (person.emails.email.length > 0) {
-                            text = text + ", " + person.emails.email[0].email;
+                            text = text + "; " + person.emails.email[0].email;
                         }
                         var newOption = new Option(text, id, true, true);
                         newOption.title = 'Open in new tab to view ORCID page';
                         $('#' + selectId).append(newOption).trigger('change');
                     },
-                    failure: function(jqXHR, textStatus, errorThrown) {
+                    error: function(jqXHR, textStatus, errorThrown) {
                         if (jqXHR.status != 404) {
                             console.error("The following error occurred: " + textStatus, errorThrown);
                         }
                     }
                 });
             } else {
-                //If the initial value is not an ORCID (legacy, or if tags are enabled), just display it as is 
+                //If the initial value is not an ORCID (legacy, or if tags are enabled), just display it as is
+                if(Object.keys(managedFields).length > 0) {
+                    //Handle managed fields
+
+                    $(parent).find("[data-cvoc-managed-field='" + managedFields.idType + "']").parent().show();
+                    $(personInput).parent().show();
+                    id = $(parent).find("input[data-cvoc-managed-field='" + managedFields.personName + "']").val();
+                }
+
                 var newOption = new Option(id, id, true, true);
                 $('#' + selectId).append(newOption).trigger('change');
             }
@@ -203,10 +244,48 @@ function updatePeopleInputs() {
                     //Tags are allowed, so just enter the text as is
                     $("input[data-person='" + num + "']").val(data.id);
                 }
+
+                //In the multi-field case, we should also fill in the other hidden managed fields
+                if (hasParentField) {
+                    var parent = $("input[data-person='" + num + "']").closest("[data-cvoc-parentfield='" + parentField + "']");
+                    let isOrcid = data.text.includes(';');
+                    for (var key in managedFields) {
+                        if (key == 'personName') {
+                            $(parent).find("input[data-cvoc-managed-field='" + managedFields[key] + "']").attr('value',data.text.split(";", 1)[0] );
+                        } else if (key == 'idType') {
+                            $(parent).find("input[data-cvoc-managed-field='" + managedFields[key] + "']").attr('value', isOrcid ? 'ORCID':'');
+                        }
+                    }
+                    if(Object.keys(managedFields).length > 0) {
+                        if(isOrcid) {
+                             //Hide managed fields
+                             $(parent).find("input[data-person='" + num + "']").parent().hide();
+                             $(parent).find("[data-cvoc-managed-field='" +  managedFields.idType + "']").parent().hide();
+                        } else {
+                            //Hide managed fields
+                            let idField =  $(parent).find("input[data-person='" + num + "']");
+                            idField.val('');
+                            idField.parent().show();
+                            $(parent).find("[data-cvoc-managed-field='" + managedFields.idType + "']").parent().show();
+                        }
+                    }
+                }
             });
             //When a selection is cleared, clear the hidden input
             $('#' + selectId).on('select2:clear', function(e) {
+              $(this).empty().trigger("change");
                 $("input[data-person='" + num + "']").attr('value', '');
+                //In the multi-field case, we should also clear the other hidden managed fields
+                if (hasParentField) {
+                    var parent = $("input[data-person='" + num + "']").closest("[data-cvoc-parentfield='" + parentField + "']");
+                    for (var key in managedFields) {
+                        if (key == 'personName') {
+                            $(parent).find("input[data-cvoc-managed-field='" + managedFields[key] + "']").attr('value','');
+                        } else if (key == 'idType') {
+                            $(parent).find("input[data-cvoc-managed-field='" + managedFields[key] + "']").attr('value', '');
+                        }
+                    }
+                }
             });
         }
     });
