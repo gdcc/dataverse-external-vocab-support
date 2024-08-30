96b414bbad31ea0478bfe7406bfefe6f8852fa2a
Jim Myers <qqmyers@hotmail.com>
2024-08-30 17:01:12 -0400

    more fixes, clear working, fix race in aboveFold

--- packages/local_contexts/local_contexts.js
+++ packages/local_contexts/local_contexts.js
@@ -25,38 +25,37 @@ async function cvoc_lc_viewProject() {
       projectField.addClass("expanded")
       const fullUrl = projectField.text()
       const serviceUrl = projectField.attr("data-cvoc-service-url")
-
-      //Dataverse-specific, temporary - see below
-      aboveFoldServiceUrl = serviceUrl
-
       const project = await cvoc_lc_LoadOrFetch(fullUrl, serviceUrl)
       let lcContainerElement = cvoc_lc_buildLCProjectPopup(project)
       if (!$.isEmptyObject(lcContainerElement)) {
         projectField.html(lcContainerElement)
       }
+      //Dataverse-specific, temporary - see below
+      aboveFoldServiceUrl = projectField.attr("data-cvoc-service-url")
     }
-  // Temporary - Dataverse doesn't currently support managing a field 'above the fold' on the dataset page
-  // To make this script work for the LCProjectUrl field in the LocalContextsCVoc metadatablock when the
-  // :CustomDatasetSummaryFields setting includes it, the following section looks for the above-fold field
-  // using a Dataverse-specific mechanism. This mechanism also assumes that only one field is using this
-  // script/that all use the same data-cvoc-service-url. The aboveFoldServiceUrl variable is set to that value
-  // (since the field found here does not include data-cvoc-* attributes at all)
-  //
-  // If/when Dataverse is enhanced to annotate this above-fold field appropriately, this section won't be needed.
-  //
-  const aboveFold = $('#LCProjectUrl')
-  if (aboveFold.length === 1) {
-    const td = aboveFold.children('td')
-    if (!td.hasClass('expanded')) {
-      var url = td.children('a').text()
-      console.log('Found: ' + url)
-      td.addClass('expanded')
-      const project = await cvoc_lc_LoadOrFetch(url, aboveFoldServiceUrl)
-      //console.log(JSON.stringify(project));
-      let lcContainerElement = cvoc_lc_buildLCProjectPopup(project, 60)
-      //console.log(lcContainerElement);
-      if (!$.isEmptyObject(lcContainerElement)) {
-        td.html(lcContainerElement)
+    // Temporary - Dataverse doesn't currently support managing a field 'above the fold' on the dataset page
+    // To make this script work for the LCProjectUrl field in the LocalContextsCVoc metadatablock when the
+    // :CustomDatasetSummaryFields setting includes it, the following section looks for the above-fold field
+    // using a Dataverse-specific mechanism. This mechanism also assumes that only one field is using this
+    // script/that all use the same data-cvoc-service-url. The aboveFoldServiceUrl variable is set to that value
+    // (since the field found here does not include data-cvoc-* attributes at all)
+    //
+    // If/when Dataverse is enhanced to annotate this above-fold field appropriately, this section won't be needed.
+    //
+    const aboveFold = $('#LCProjectUrl')
+    if (aboveFold.length === 1) {
+      const td = aboveFold.children('td')
+      if (!td.hasClass('expanded')) {
+        var url = td.children('a').text()
+        console.log('Found: ' + url)
+        td.addClass('expanded')
+        const project = await cvoc_lc_LoadOrFetch(url, aboveFoldServiceUrl)
+        //console.log(JSON.stringify(project));
+        let lcContainerElement = cvoc_lc_buildLCProjectPopup(project, 60)
+        //console.log(lcContainerElement);
+        if (!$.isEmptyObject(lcContainerElement)) {
+          td.html(lcContainerElement)
+        }
       }
     }
   }
@@ -64,72 +63,120 @@ async function cvoc_lc_viewProject() {
 
 async function cvoc_lc_editProject() {
   var projectInputs = $(cvoc_lc_projectInputSelector)
-  //This is a for loop instead of projectInputs.each() because async functions are called (not allowed with .each)
+  // This script is intended to work with one single valued metadata field on the page,
+  // but an input can appear in other places than metadata edit (e.g. advanced search)
+  // In the current UI, I don't think both of these fields are ever displayed together,
+  // but to be safe in that case, this uses a loop
+
+  // This is a for loop instead of projectInputs.each() because async functions are called (not allowed with .each)
   for (let i = 0; i < projectInputs.length; i++) {
     const projectInput = $(projectInputs[i])
-    projectInput.hide()
-    const serviceUrl = projectInput.attr('data-cvoc-service-url')
-    let select_ = document.createElement("select")
-    select_.id = "localcontextsProjectInputSelector"
-    select_.classList = "form-control add-resource select2"
-    select_.setAttribute("aria-hidden", true)
-    select_.setAttribute("tabindex", -1)
-    projectInput.after(select_)
-    let placeholder = ""
-    if (projectInput[0].value !== "") {
-      const project = await cvoc_lc_LoadOrFetch(projectInput[0].value, serviceUrl)
-      // console.log(project)
-      placeholder = project.title
-    } else {
-      placeholder = "Search for a project by name or paste the exact project ID"
-    }
-    // todo we have: projectInput.value
-    $(select_).select2({
-      tags: $(projectInput).attr('data-cvoc-allowfreetext'),
-      allowClear: true,
-      placeholder: placeholder,
-      minimumInputLength: cvoc_lc_seach_minimumInputLength,
-      ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
-        url: (params) => {
-          // check if the user posted a uuid (lc project id) and
-          const uuid_regex = new RegExp("([a-f 0-9]{8})-([a-f 0-9]{4})-([a-f 0-9]{4})-([a-f 0-9]{4})-([a-f 0-9]{12})")
-          if (uuid_regex.test(params.term)) {
-            //await get_or_fetch(params.term)
-            return `${serviceUrl}api/v1/projects/${params.term}`
-          } else {
-            return `${serviceUrl}api/v1/projects/?search=${params.term}`
-          }
-        },
-        data: {},
-        dataType: 'json',
-        delay: cvoc_lc_search_delay,
-        processResults: function(data, page) { // parse the results into the format expected by Select2.
-          // console.log("processResults", data)
-          // check if we did the search by uuid
-          if (data.results === undefined && data.unique_id !== undefined) {
-            return {
-              results: [{ id: data.unique_id, text: data.title }]
-            }
-          }
-          // normal search results
-          return {
-            results: data.results.map(e => ({
-              id: e.unique_id, text: e.title
-            }))
-          }
-        },
-        cache: true
+    if (!projectInput.attr('data-lc')) {
+      // Random identifier
+      const num = Math.floor(Math.random() * 100000000000);
+      projectInput.attr('data-lc', num);
+      projectInput.hide()
+      const serviceUrl = projectInput.attr('data-cvoc-service-url')
+      const baseUrl = `${serviceUrl}projects/`
+      const selectId = "LCAddSelect_" + num;
+      projectInput.after(
+        '<select id=' + selectId + ' class="form-control add-resource select2" tabindex="-1" aria-hidden="true">');
+      if (projectInput[0].value !== "") {
+        const project = await cvoc_lc_LoadOrFetch(projectInput[0].value, serviceUrl)
+        // console.log(project)
       }
-    })
+      var placeholder = projectInput.attr('data-cvoc-placeholder')
+      if (typeof placeholder === "undefined") {
+        placeholder = "Search for a project by name or paste the exact project ID"
+      }
+      // todo we have: projectInput.value
+      $("#" + selectId).select2({
+        theme: "classic",
+        tags: $(projectInput).attr('data-cvoc-allowfreetext'),
+        allowClear: true,
+        placeholder: placeholder,
+        delay: cvoc_lc_search_delay,
+        minimumInputLength: cvoc_lc_seach_minimumInputLength,
+        templateResult: function(item) {
+          // No need to template the searching text
+          if (item.loading) {
+            return item.text;
+          }
+          // markMatch bolds the search term if/where it appears in
+          // the result
+          var $result = markMatch2(item.text, term);
+          return $result;
+        },
 
+        ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+          url: (params) => {
+            // check if the user posted a uuid (lc project id) and
+            const uuid_regex = new RegExp("([a-f 0-9]{8})-([a-f 0-9]{4})-([a-f 0-9]{4})-([a-f 0-9]{4})-([a-f 0-9]{12})")
+            if (uuid_regex.test(params.term)) {
+              //await get_or_fetch(params.term)
+              return `${serviceUrl}api/v1/projects/${params.term}`
+            } else {
+              return `${serviceUrl}api/v1/projects/?search=${params.term}`
+            }
+          },
+          data: function(params) {
+            term = params.term;
+            if (!term) {
+              term = "";
+              console.log("no term!");
+            }
+            var query = {
+              query: term,
+            }
+            return query;
+          },
+          dataType: 'json',
 
-    $(select_).on('select2:select', function(e) {
-      // console.log("select...")
-      let data = e.params.data
-      // console.log(data)
-      projectInput.val(`${serviceUrl}projects/${data.id}`)
-      cvoc_lc_LoadOrFetch(data.id, serviceUrl)
-    })
+          processResults: function(data, page) { // parse the results into the format expected by Select2.
+            // console.log("processResults", data)
+            // check if we did the search by uuid
+            if (data.results === undefined && data.unique_id !== undefined) {
+              return {
+                results: [{ id: data.unique_id, text: data.title }]
+              }
+            }
+            // normal search results
+            return {
+              results: data.results.map(e => ({
+                id: e.unique_id, text: e.title
+              }))
+            }
+          },
+          cache: true
+        }
+      })
+      // If the input has a value already, format it the same way as if it
+      // were a new selection
+      var id = projectInput.val();
+      if (id.startsWith(baseUrl)) {
+        var data = await cvoc_lc_LoadOrFetch(id, serviceUrl)
+        var name = data.title;
+        //Display the name and id number in the selection menu
+        var newOption = new Option(name, id, true, true);
+        $('#' + selectId).append(newOption).trigger('change');
+      } else {
+        // If the initial value is not a project, just display it as is
+        var newOption = new Option(id, id, true, true);
+        newOption.altNames = ['No LocalContexts Entry'];
+        $('#' + selectId).append(newOption).trigger('change');
+      }
+
+      $("#" + selectId).on('select2:select', function(e) {
+        // console.log("select...")
+        let data = e.params.data
+        projectInput.val(`${serviceUrl}projects/${data.id}`)
+        cvoc_lc_LoadOrFetch(data.id, serviceUrl)
+      })
+      // When a selection is cleared, clear the hidden input
+      $('#' + selectId).on('select2:clear', function(e) {
+        $("input[data-lc='" + num + "']").attr('value', '');
+      });
+    }
   }
 }
 
@@ -193,3 +240,4 @@ async function cvoc_lc_LoadOrFetch(fullUrl, serviceUrl) {
   sessionStorage.setItem(lc_uuid, JSON.stringify(project))
   return Promise.resolve(project)
 }
+
